d1717af059fbe1f7f0bc9ff1744d87c156c09699
garyantonyo <garyantonyo@gmail.com>
2021-06-21 16:53:38 -0400

    renamed and messed with block models

---
+++ src/main/java/moe/timgor/kilnmod/BlockInit.java
@@ -0,0 +1,21 @@
+package moe.timgor.kilnmod;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class BlockInit {
+    public static final DeferredRegister<Block> Blocks = DeferredRegister.create(ForgeRegistries.BLOCKS, KilnMod.MOD_ID);
+    public static final DeferredRegister<Item> Items = DeferredRegister.create(ForgeRegistries.ITEMS, KilnMod.MOD_ID);
+
+    public static final RegistryObject<Block> kiln_block = Blocks.register("kiln_block", () -> new Block(AbstractBlock.Properties.of(Material.STONE, MaterialColor.STONE)));
+    public static final RegistryObject<Item> kiln_item = Items.register("kiln_item", () -> new BlockItem(kiln_block.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
+
+}
--- src/main/java/moe/timgor/kilnmod/KilnMod.java
+++ src/main/java/moe/timgor/kilnmod/KilnMod.java
@@ -1,33 +1,23 @@
-package moe.timgor.kiln;
+package moe.timgor.kilnmod;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(Kiln.MOD_ID)
-public class Kiln
+@Mod(KilnMod.MOD_ID)
+public class KilnMod
 {
     // Directly reference a log4j logger.
     public static final Logger LOGGER = LogManager.getLogger();
-    public static final String MOD_ID = "kiln";
+    public static final String MOD_ID = "kilnmod";
 
-    public Kiln() {
+    public KilnMod() {
         // Register the setup method for modloading
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         bus.addListener(this::setup);
