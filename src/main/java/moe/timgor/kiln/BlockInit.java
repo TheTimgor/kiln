fa02c1fe80fa42a2eb190ee1128048053529f179
timgor <egor.vladone@gmail.com>
2021-06-21 16:10:54 -0400

    trying to add textures

--- src/main/java/moe/timgor/kiln/BlockInit.java
+++ src/main/java/moe/timgor/kiln/BlockInit.java
@@ -16,6 +16,6 @@ public class BlockInit {
     public static final DeferredRegister<Item> Items = DeferredRegister.create(ForgeRegistries.ITEMS, Kiln.MOD_ID);
 
     public static final RegistryObject<Block> kiln_block = Blocks.register("kiln", () -> new Block(AbstractBlock.Properties.of(Material.STONE, MaterialColor.STONE)));
-    public static final RegistryObject<Item> kiln_item = Items.register("kiln", () -> new BlockItem(kiln_block.get(), new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> kiln_item = Items.register("kiln", () -> new BlockItem(kiln_block.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
 
 }
