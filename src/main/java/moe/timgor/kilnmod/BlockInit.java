57c7da5aad25c3009f01e9ba4a4ffab9609cb203
timgor <egor.vladone@gmail.com>
2021-06-21 21:59:26 -0400

    made kilnblock extend furnaceblock

--- src/main/java/moe/timgor/kilnmod/BlockInit.java
+++ src/main/java/moe/timgor/kilnmod/BlockInit.java
@@ -15,7 +15,7 @@ public class BlockInit {
     public static final DeferredRegister<Block> Blocks = DeferredRegister.create(ForgeRegistries.BLOCKS, KilnMod.MOD_ID);
     public static final DeferredRegister<Item> Items = DeferredRegister.create(ForgeRegistries.ITEMS, KilnMod.MOD_ID);
 
-    public static final RegistryObject<Block> kiln_block = Blocks.register("kiln_block", () -> new Block(AbstractBlock.Properties.of(Material.STONE, MaterialColor.STONE)));
+    public static final RegistryObject<Block> kiln_block = Blocks.register("kiln_block", () -> new KilnBlock(AbstractBlock.Properties.of(Material.STONE, MaterialColor.STONE)));
     public static final RegistryObject<Item> kiln_item = Items.register("kiln_item", () -> new BlockItem(kiln_block.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
 
 }
---
+++ src/main/java/moe/timgor/kilnmod/KilnBlock.java
@@ -0,0 +1,27 @@
+package moe.timgor.kilnmod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FurnaceBlock;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.tileentity.FurnaceTileEntity;
+
+public class KilnBlock extends FurnaceBlock {
+    public KilnBlock(Properties p_i48393_1_) {
+        super(p_i48393_1_);
+    }
+//
+//    public KilnBlock(Properties p_i48440_1_) {
+//        super(p_i48440_1_);
+//    }
+//
+//    public boolean hasTileEntity(BlockState state){
+//        return true;
+//    }
+//
+//    public TileEntity createTileEntity(BlockState state, IBlockReader world){
+//        return new FurnaceTileEntity();
+//    }
+
+}
